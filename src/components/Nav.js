dc61ac5166c041819c9eef87d4b6d0745ccbddd3
hayoungipnida <fruitdelapassi00n@gmail.com>
2026-06-24 15:56:26 +0900

    오류수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ import styles from "./Nav.module.css";
 export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <a href="#" className={styles.navLink}>works</a>
-      <a href="#" className={styles.navLink}>skill</a>
-      <a href="#" className={styles.navLink}>contact</a>
+      <a href="/" className={styles.navLink}>works</a>
+      <a href="/" className={styles.navLink}>skill</a>
+      <a href="/" className={styles.navLink}>contact</a>
     </nav>
   );
 }
